113b943db81ba360a40b5f1c33cdb845263cf014
Adam Palay <adam@edx.org>
2015-09-11 09:39:18 -0400

    fix orphan deletion for split courses
    refactor delete orphan tests and make more robust

--- cms/djangoapps/contentstore/tests/test_orphan.py
+++ cms/djangoapps/contentstore/tests/test_orphan.py
@@ -6,8 +6,19 @@ import ddt
 from contentstore.tests.utils import CourseTestCase
 from student.models import CourseEnrollment
 from contentstore.utils import reverse_course_url
+from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
 from xmodule.modulestore import ModuleStoreEnum
-from xmodule.modulestore.tests.factories import CourseFactory
+from xmodule.modulestore.split_mongo import BlockKey
+import ddt
+import itertools
+
+from opaque_keys.edx.locator import CourseLocator
+
+ORPHAN_TYPES = itertools.product(
+    [ModuleStoreEnum.BranchName.published, ModuleStoreEnum.BranchName.draft],
+    [True, False],
+    [True, False],
+)
 
 
 class TestOrphanBase(CourseTestCase):
@@ -73,6 +84,155 @@ class TestOrphan(TestOrphanBase):
     """
     Test finding orphans via view and django config
     """
+    @ddt.data(ModuleStoreEnum.Type.mongo, ModuleStoreEnum.Type.split)
+    def test_get_orphans(self, default_store):
+        """
+        Test that the orphan handler finds a course's orphans
+        """
+        course = self.create_course_with_orphans(default_store)
+        orphan_url = reverse_course_url('orphan_handler', course.id)
+        orphans = json.loads(
+            self.client.get(
+                orphan_url,
+                HTTP_ACCEPT='application/json'
+            ).content
+        )
+        self.assertEqual(len(orphans), 3, "Wrong # {}".format(orphans))
+        location = course.location.replace(category='chapter', name='OrphanChapter')
+        self.assertIn(location.to_deprecated_string(), orphans)
+        location = course.location.replace(category='vertical', name='OrphanVert')
+        self.assertIn(location.to_deprecated_string(), orphans)
+        location = course.location.replace(category='html', name='OrphanHtml')
+        self.assertIn(location.to_deprecated_string(), orphans)
+
+    @ddt.data(ModuleStoreEnum.Type.mongo, ModuleStoreEnum.Type.split)
+    def test_delete_orphans(self, default_store):
+        """
+        Test that the orphan handler can delete a course's orphans
+        """
+        course = self.create_course_with_orphans(default_store)
+        orphan_url = reverse_course_url('orphan_handler', course.id)
+
+        self.assertEqual(len(self.store.get_items(course.id)), 12)
+        self.client.delete(orphan_url)
+        orphans = json.loads(
+            self.client.get(orphan_url, HTTP_ACCEPT='application/json').content
+        )
+        self.assertEqual(len(orphans), 0, "Orphans not deleted {}".format(orphans))
+
+        # make sure that any children with one orphan parent and one non-orphan
+        # parent are not deleted
+        self.assertTrue(self.store.has_item(course.id.make_usage_key('html', "multi_parent_html")))
+        self.assertFalse(self.store.has_item(course.id.make_usage_key('vertical', "OrphanVert")))
+        self.assertFalse(self.store.has_item(course.id.make_usage_key('chapter', "OrphanChapter")))
+        self.assertFalse(self.store.has_item(course.id.make_usage_key('html', "OrphanHtml")))
+
+        # make sure that exactly the three orphans items were deleted
+        self.assertEqual(len(self.store.get_items(course.id)), 9)
+
+    @ddt.data(ModuleStoreEnum.Type.mongo, ModuleStoreEnum.Type.split)
+    def test_not_permitted(self, default_store):
+        """
+        Test that auth restricts get and delete appropriately
+        """
+        course = self.create_course_with_orphans(default_store)
+        orphan_url = reverse_course_url('orphan_handler', course.id)
+        test_user_client, test_user = self.create_non_staff_authed_user_client()
+        CourseEnrollment.enroll(test_user, course.id)
+        response = test_user_client.get(orphan_url)
+        self.assertEqual(response.status_code, 403)
+        response = test_user_client.delete(orphan_url)
+        self.assertEqual(response.status_code, 403)
+
+
+@ddt.ddt
+class TestSplitOrphan(TestOrphanBase):
+    """
+    Tests for split-specific orphan scenarios
+    """
+    def create_course_with_orphan_on_branch(self, branch, direct_only):
+        """
+        branch is the branch to create the orphan on
+        direct_only is whether or not its parent is in a direct only category
+        """
+        course = CourseFactory.create(default_store=ModuleStoreEnum.Type.split)
+        chapter = ItemFactory.create(category='chapter', parent=course)
+        sequential = ItemFactory.create(category='sequential', parent=chapter)
+        vertical = ItemFactory.create(category='vertical', parent=sequential)
+        html = ItemFactory.create(category='html', parent=vertical)
+
+        branch_explicit_course_key = course.id.for_branch(branch)
+        store = self.store._get_modulestore_by_type(ModuleStoreEnum.Type.split)
+
+        # Now, we're going to remove the children from one of the blocks on the branch
+        # we want to make the orphans on
+        block = chapter if direct_only else vertical
+
+        original_structure = store._lookup_course(branch_explicit_course_key).structure
+        new_structure = store.version_structure(
+            branch_explicit_course_key, original_structure, self.user.id
+        )
+
+        # remove children
+        block_key = BlockKey.from_usage_key(block.location)
+        new_structure['blocks'][block_key].fields['children'] = []
+
+        new_id = new_structure['_id']
+
+        index_entry = store._get_index_if_valid(branch_explicit_course_key)
+        store._update_head(course.id, index_entry, branch_explicit_course_key.branch, new_structure['_id'])
+
+        store.update_structure(branch_explicit_course_key, new_structure)
+
+        return course
+
+    def create_course_with_orphan_on_branch_DNE_other_branch(self, branch, direct_only):
+        """
+        Tests that if there are orphans only on the published branch,
+        running delete orphans with a course key that specifies
+        the published branch will delete the published orphan
+
+        branch is a BranchName (like draft-branch or published-branch)
+        """
+        course = CourseFactory.create(default_store=ModuleStoreEnum.Type.split)
+        # create an orphan
+        category = 'chapter' if direct_only else 'html'
+        orphan = self.store.create_item(self.user.id, course.id, category, "OrphanHtml")
+        self.store.publish(orphan.location, self.user.id)
+
+        # grab the branch of the course we want to make the orphan on
+        published_branch = course.id.for_branch(
+            branch
+        )
+
+        # assert that this orphan is present in both branches
+        self.assertOrphanCount(course.id, 1)
+        self.assertOrphanCount(published_branch, 1)
+
+        # Delete the orphan from the other branch without
+        # auto-publishing the change to the this draft.
+        # Now, the orphan will only be on this branch
+        revision = None
+        if branch == ModuleStoreEnum.BranchName.draft:
+            revision = ModuleStoreEnum.RevisionOption.published_only
+
+        self.store.delete_item(
+            orphan.location,
+            self.user.id,
+            revision=revision,
+            skip_auto_publish=True,
+        )
+        return course
+
+    def assertOrphanCount(self, course_key, number):
+        self.assertEqual(len(self.store.get_orphans(course_key)), number)
+
+
+@ddt.ddt
+class TestOrphan(TestOrphanBase):
+    """
+    Test finding orphans via view and django config
+    """
 
     @ddt.data(ModuleStoreEnum.Type.split, ModuleStoreEnum.Type.mongo)
     def test_get_orphans(self, default_store):
@@ -114,8 +274,91 @@ class TestOrphan(TestOrphanBase):
         # parent are not deleted
         self.assertTrue(self.store.has_item(course.id.make_usage_key('html', "multi_parent_html")))
 
+    def test_split_mongo_orphan_delete(self):
+        self.course = self.create_course_with_orphans(ModuleStoreEnum.Type.split)
+        store = self.store._get_modulestore_by_type(ModuleStoreEnum.Type.split)
+        self.assertOrphanCount(self.course.id, 3)
+        self.assertOrphanCount(self.course.id.for_branch('published-branch'), 3)
+        store.delete_orphans(self.course.id, self.user.id)
+        self.assertOrphanCount(self.course.id, 0)
+        self.assertOrphanCount(self.course.id.for_branch('published-branch'), 0)
+
+    @ddt.data(*ORPHAN_TYPES)
+    @ddt.unpack
+    def test_split_mongo_orphan_delete_special_cases(
+        self,
+        branch,
+        direct_only,
+        exists_on_other_branch,
+    ):
+        """
+        branch: which branch the orphan is on
+        direct_only: whether or not the orphan's parent is a "direct only category)
+        exists_on_other_branch: if the orphan has a corresponding xblock
+          that exists on the other branch
+        """
+        draft_orphans, published_orphans = 1, 0
+        if branch == ModuleStoreEnum.BranchName.published:
+            draft_orphans, published_orphans = 0, 1
+
+        course = self.create_course_with_orphan_on_branch_DNE_other_branch(branch, direct_only)
+        if exists_on_other_branch:
+            course = self.create_course_with_orphan_on_branch(branch, direct_only)
+
+        store = self.store._get_modulestore_by_type(ModuleStoreEnum.Type.split)
+
+        self.assertOrphanCount(course.id, draft_orphans)
+        self.assertOrphanCount(course.id.for_branch('published-branch'), published_orphans)
+
+        store.delete_orphans(course.id, self.user.id)
+
+        self.assertOrphanCount(course.id, 0)
+        self.assertOrphanCount(course.id.for_branch('published-branch'), 0)
+
+
+    def test_split_mongo_orphan_delete_2(self):
+        course = CourseFactory.create(default_store=ModuleStoreEnum.Type.split)
+        chapter = ItemFactory.create(category='chapter', parent=course)
+        sequential = ItemFactory.create(category='sequential', parent=chapter)
+        vertical = ItemFactory.create(category='vertical', parent=sequential)
+        html = ItemFactory.create(category='html', parent=vertical)
+
+        pub = course.id.for_branch('published-branch')
+        course_id = course.id
+        store = self.store._get_modulestore_by_type('split')
+
+        chapter_key = BlockKey.from_usage_key(chapter.location)
+
+        original_structure = store._lookup_course(pub).structure
+
+        new_structure = store.version_structure(pub, original_structure, self.user.id)
+
+        new_structure['blocks'][chapter_key].fields['children'] = []
+        parent_block_keys = store._get_parents_from_structure(chapter.location, original_structure)
+        parent_block_key = parent_block_key
+        store.decache_block(chapter.location.course_key, new_id, parent_block_key)
+
+        index_entry = store._get_index_if_valid(course.id)
+        new_id = new_structure['_id']
+
+        # update index if appropriate and structures
+        store.update_structure(usage_locator.course_key, new_structure)
+
+        if index_entry is not None:
+            # update the index entry if appropriate
+            store._update_head(course.id, index_entry, 'published-branch', new_id)
+            result = usage_locator.course_key.for_version(new_id)
+        else:
+            result = CourseLocator(version_guid=new_id)
+
+
+
+
+        # import ipdb; ipdb.set_trace()
+
+
     @ddt.data(ModuleStoreEnum.Type.split, ModuleStoreEnum.Type.mongo)
-    def test_not_permitted(self, default_store):
+    def test_not_permitted(self):
         """
         Test that auth restricts get and delete appropriately
         """
--- common/lib/xmodule/xmodule/modulestore/split_mongo/split_draft.py
+++ common/lib/xmodule/xmodule/modulestore/split_mongo/split_draft.py
@@ -175,7 +175,7 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
             self._auto_publish_no_children(parent_usage_key, item.location.category, user_id, **kwargs)
             return item
 
-    def delete_item(self, location, user_id, revision=None, skip_auto_publish=False, **kwargs):
+    def delete_item(self, location, user_id, revision=None, skip_auto_publish=False, force=False, delete_children=True, **kwargs):
         """
         Delete the given item from persistence. kwargs allow modulestore specific parameters.
 
@@ -211,7 +211,7 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
             for branch in branches_to_delete:
                 branched_location = location.for_branch(branch)
                 parent_loc = self.get_parent_location(branched_location)
-                SplitMongoModuleStore.delete_item(self, branched_location, user_id)
+                SplitMongoModuleStore.delete_item(self, branched_location, user_id, force, delete_children)
                 # publish parent w/o child if deleted element is direct only (not based on type of parent)
                 # publish vertical to behave more like the old mongo/draft modulestore - TNL-2593
                 if (
@@ -301,6 +301,113 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
         course_key = self._map_revision_to_branch(course_key)
         return super(DraftVersioningModuleStore, self).get_orphans(course_key, **kwargs)
 
+    def _get_orphan_data(self, draft_key, published_key):
+        """
+        Get requisite information about draft and published orphans
+        to delete them appropriately.
+        """
+        draft_orphans = self.get_orphans(draft_key)
+        published_orphans = self.get_orphans(published_key)
+
+        draft_orphan_ids = [orphan.block_id for orphan in draft_orphans]
+        published_orphan_ids = [orphan.block_id for orphan in published_orphans]
+
+        published_block_ids = [
+            block.id for block in self._lookup_course(published_key).structure['blocks']
+        ]
+
+        draft_block_ids = [
+            block.id for block in self._lookup_course(draft_key).structure['blocks']
+        ]
+
+        orphans = draft_orphans + published_orphans
+
+        return {
+            "draft_orphan_ids": draft_orphan_ids,
+            "published_orphan_ids": published_orphan_ids,
+            "draft_block_ids": draft_block_ids,
+            "published_block_ids": published_block_ids,
+            "orphans": orphans,
+        }
+
+    def delete_orphans(self, course_key, user_id, **kwargs):
+        draft_preferred_course_key = self._map_revision_to_branch(course_key)
+        published_course_key = self._map_revision_to_branch(
+            course_key, ModuleStoreEnum.RevisionOption.published_only
+        )
+
+        orphan_data = self._get_orphan_data(
+            draft_preferred_course_key, published_course_key
+        )
+        orphans = orphan_data["orphans"]
+        draft_orphan_ids = orphan_data["draft_orphan_ids"]
+        published_orphan_ids = orphan_data["published_orphan_ids"]
+        draft_block_ids = orphan_data["draft_block_ids"]
+        published_block_ids = orphan_data["published_block_ids"]
+
+        while orphans:
+            orphan = orphans.pop()
+            if orphan.block_id in (set(draft_orphan_ids) & set(published_orphan_ids)):
+                # great, delete from both groups
+                self.delete_item(
+                    orphan, user_id, revision=ModuleStoreEnum.RevisionOption.all
+                )
+
+            elif orphan.branch == ModuleStoreEnum.BranchName.draft:
+                if orphan.block_id not in published_block_ids:
+                    # not passing a revision means you delete just from the draft branch
+                    self.delete_item(orphan, user_id, skip_auto_publish=True)
+                else:
+                    # revert parent to published
+                    published_location = orphan.for_branch(
+                        ModuleStoreEnum.BranchName.published
+                    )
+                    published_block = self.get_item(published_location)
+                    published_parent = published_block.parent
+                    draft_would_be_parent = published_parent.for_branch(
+                        ModuleStoreEnum.BranchName.draft
+                    )
+                    # TODO: what do you do if draft_would_be_parent isn't in
+                    # the draft branch of the course?
+                    self.revert_single_block_to_published(draft_would_be_parent, user_id, force=True)
+
+            elif orphan.branch == ModuleStoreEnum.BranchName.published:
+                if orphan.block_id not in draft_block_ids:
+                    self.delete_item(
+                        orphan,
+                        user_id,
+                        revision=ModuleStoreEnum.RevisionOption.published_only
+                    )
+                else:
+                    draft_location = orphan.for_branch('draft-branch')
+                    draft_block = self.get_item(draft_location)
+                    draft_parent = draft_block.parent
+                    if draft_parent.category in DIRECT_ONLY_CATEGORIES:
+                        self.revert_single_block_to_published(
+                            draft_parent, user_id, force=True
+                        )
+                        self.delete_item(
+                            orphan,
+                            user_id,
+                            revision=ModuleStoreEnum.RevisionOption.all,
+                        )
+                    else:
+                        self.delete_item(
+                            orphan,
+                            user_id,
+                            revision=ModuleStoreEnum.RevisionOption.published_only
+                        )
+
+            # recalculate orphan data
+            orphan_data = self._get_orphan_data(
+                draft_preferred_course_key, published_course_key
+            )
+            orphans = orphan_data['orphans']
+            draft_orphan_ids = orphan_data["draft_orphan_ids"]
+            published_orphan_ids = orphan_data["published_orphan_ids"]
+            draft_block_ids = orphan_data["draft_block_ids"]
+            published_block_ids = orphan_data["published_block_ids"]
+
     def fix_not_found(self, course_key, user_id):
         """
         Fix any children which point to non-existent blocks in the course's published and draft branches
@@ -379,7 +486,13 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
             self.delete_item(location, user_id, revision=ModuleStoreEnum.RevisionOption.published_only)
             return self.get_item(location.for_branch(ModuleStoreEnum.BranchName.draft), **kwargs)
 
-    def revert_to_published(self, location, user_id):
+    def revert_to_published(self, location, user_id=None):
+        return self._revert_to_published(location, user_id)
+
+    def revert_single_block_to_published(self, location, user_id, force=False):
+        self._revert_to_published(location, user_id, recursive=False, force=True)
+
+    def _revert_to_published(self, location, user_id, recursive=True, force=False):
         """
         Reverts an item to its last published version (recursively traversing all of its descendants).
         If no published version exists, a VersionConflictError is thrown.
@@ -389,7 +502,7 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
 
         :raises InvalidVersionError: if no published version exists for the location specified
         """
-        if location.category in DIRECT_ONLY_CATEGORIES:
+        if location.category in DIRECT_ONLY_CATEGORIES and not force:
             return
 
         draft_course_key = location.course_key.for_branch(ModuleStoreEnum.BranchName.draft)
@@ -411,12 +524,16 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
             new_structure = self.version_structure(draft_course_key, draft_course_structure, user_id)
 
             # remove the block and its descendants from the new structure
-            self._remove_subtree(BlockKey.from_usage_key(location), new_structure['blocks'])
+            if recursive:
+                self._remove_subtree(BlockKey.from_usage_key(location), new_structure['blocks'])
+            else:
+                del new_structure['blocks'][BlockKey.from_usage_key(location)]
 
             # copy over the block and its descendants from the published branch
             def copy_from_published(root_block_id):
                 """
-                copies root_block_id and its descendants from published_course_structure to new_structure
+                copies root_block_id and its descendants (if `recursive`) from
+                published_course_structure to new_structure
                 """
                 self._update_block_in_structure(
                     new_structure,
@@ -424,6 +541,8 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
                     self._get_block_from_structure(published_course_structure, root_block_id)
                 )
                 block = self._get_block_from_structure(new_structure, root_block_id)
+                if not recursive:
+                    return
                 for child_block_id in block.fields.get('children', []):
                     copy_from_published(child_block_id)
 
@@ -463,6 +582,18 @@ class DraftVersioningModuleStore(SplitMongoModuleStore, ModuleStoreDraftAndPubli
         Returns whether this xblock has a published version (whether it's up to date or not).
         """
         return self._get_head(xblock, ModuleStoreEnum.BranchName.published) is not None
+
+    def _get_published_version(self, xblock):
+        """
+        Returns the xblock's published version if it's there
+        """
+        return self._get_head(xblock, ModuleStoreEnum.BranchName.published)
+
+    def _get_draft_version(self, xblock):
+        """
+        Returns the xblock's draft version if it's there
+        """
+        return self._get_head(xblock, ModuleStoreEnum.BranchName.draft)
 
     def convert_to_draft(self, location, user_id):
         """
